134b142989ecabe15a7773bbe96899b46889cc55
hannah-hashemi <hannahhashemib@gmail.com>
2024-07-06 01:23:34 +0330

    feat : add report file

--- app/src/main/java/com/example/barium/MainActivity.kt
+++ app/src/main/java/com/example/barium/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.barium
 
 import android.Manifest
-import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
 import android.telephony.SmsManager
